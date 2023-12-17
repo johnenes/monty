8bdbf40ed0adf5bf63c190016b42fcfa3cc2bab1
johnenes <johneneojo1@gmail.com>
2023-12-17 13:41:53 -0800

    execute.c

--- execute.c
+++ execute.c
@@ -1,5 +1,4 @@
 #include "monty.h"
-
 /**
 * execute - executes the opcode
 * @stack: head linked list - stack
@@ -8,43 +7,46 @@
 * @content: line content
 * Return: no return
 */
-
-int execute(char *content, stack_t **stack, unsigned int counter, FILE * file)
+int execute(char *content, stack_t **stack, unsigned int counter, FILE *file)
 {
 	instruction_t opst[] = {
-		{"push", f_push}, {"pall", f_pall}, {"pint",f_pint},
-		{"swap", f_swap},
-		{"add", f_add},
-		{"pop", f_pop},
-		{"mul", f_mul}
-		
-
-	};
+				{"push", f_push}, {"pall", f_pall}, {"pint", f_pint},
+				{"pop", f_pop},
+				{"swap", f_swap},
+				{"add", f_add},
+				{"nop", f_nop},
+				{"sub", f_sub},
+				{"div", f_div},
+				{"mul", f_mul},
+				{"mod", f_mod},
+				{"pchar", f_pchar},
+				{"pstr", f_pstr},
+				{"rotl", f_rotl},
+				{"rotr", f_rotr},
+				{"queue", f_queue},
+				{"stack", f_stack},
+				{NULL, NULL}
+				};
 	unsigned int i = 0;
 	char *op;
 
-	op = strtok(content, "\n\t");
+	op = strtok(content, " \n\t");
 	if (op && op[0] == '#')
 		return (0);
-
-	bus.arg = strtok(NULL, "\n\t");
-	while(opst[i].opcode && op)
+	bus.arg = strtok(NULL, " \n\t");
+	while (opst[i].opcode && op)
 	{
 		if (strcmp(op, opst[i].opcode) == 0)
-		{
-			opst[i].f(stack, counter);
+		{	opst[i].f(stack, counter);
 			return (0);
 		}
 		i++;
 	}
 	if (op && opst[i].opcode == NULL)
-	{
-		fprintf(stderr, "L%d: unknow instruction %s\n",counter , op);
+	{ fprintf(stderr, "L%d: unknown instruction %s\n", counter, op);
 		fclose(file);
 		free(content);
 		free_stack(*stack);
-		exit(EXIT_FAILURE);
-	}
+		exit(EXIT_FAILURE); }
 	return (1);
 }
-
